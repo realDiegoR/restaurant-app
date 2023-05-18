4bdfe9d4c3833b057e0539123ef425781ce5908f
realdiegor <diegoreyes150103@gmail.com>
2023-05-17 22:15:47 -0400

    chore: install next and dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { Locale } from "./context/Locale";
-import { App } from "./pages/App";
-import "./styles/main.scss";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { Locale } from './context/Locale';
+import { App } from './pages/App';
+import './styles/main.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<StrictMode>
 		<Locale>
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+	return <div>aaa</div>;
+};
+
+export default Home;
